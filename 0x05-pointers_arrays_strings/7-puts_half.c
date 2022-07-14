48b72b4c151d5932c5762ef0066ed7779f61e066
root </obichimadan@gmail.com>
2022-07-14 01:16:26 -0700

    All

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -18,8 +18,9 @@ void puts_half(char *str)
 	if (len % 2 == 0)
 	{
 		for (i = len / 2; str[i] != '\0'; i++)
-	{
+		{
 		_putchar(str[i]);
+		}
 	}
 	else if (len % 2)
 	{
@@ -27,5 +28,6 @@ void puts_half(char *str)
 		{
 			_putchar(str[n + 1]);
 		}
+	}
 		_putchar('\n');
 }
